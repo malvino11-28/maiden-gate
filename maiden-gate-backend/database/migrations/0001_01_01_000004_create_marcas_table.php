ea57746580462eba6c10c34d9fab9ead0bb87289
Malvino <149816852+malvino11-28@users.noreply.github.com>
2026-05-14 09:01:13 -0300

    fix: conserta sintaxe do migration marcas
    feat: adiciona migrations das skills

--- maiden-gate-backend/database/migrations/0001_01_01_000004_create_marcas_table.php
+++ maiden-gate-backend/database/migrations/0001_01_01_000004_create_marcas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new classes extens Migration {
+return new class extends Migration {
     public function up(): void {
         Schea::create('marcas', function(Blueprint $table) {
 
