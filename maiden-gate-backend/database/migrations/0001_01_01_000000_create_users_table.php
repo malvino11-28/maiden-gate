8d168cfb134e72b1ff24c587f59ba5fb731794aa
Malvino <malvinogarcia2@gmail.com>
2026-06-02 16:41:10 -0600

    feat: cria migration de campanha
    chore: atualiza MER
    
    refactor: arruma nome dos enums da migration player

--- maiden-gate-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ maiden-gate-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum('type', ['Mestre', 'Jogador']);
+            $table->enum('type', ['master', 'player']);
             $table->string('name')->unique();
             $table->string('password');
             $table->rememberToken();
