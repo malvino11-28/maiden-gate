c3aaddd2221e2666f69a1c0a9ba969cc039c3e16
Malvino <malvinogarcia2@gmail.com>
2026-06-02 18:37:31 -0600

    feat: cria migration de usuario de campanha
    feat: cria migration de localizações importantes da campanha
    
    feat: cria migration de lore events

---
+++ maiden-gate-backend/database/migrations/2026_06_02_231427_create_lore_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lore_events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("campaign_id");
+            $table->string("title");
+            $table->text("desc");
+            $table->string("chronology");
+            $table->string("event_date")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lore_events');
+    }
+};
