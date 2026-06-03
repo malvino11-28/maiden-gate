61d7f9f5b68b43ae3bc71118713a924e1b833546
Malvino <malvinogarcia2@gmail.com>
2026-06-03 09:17:36 -0600

    feat: adiciona arquivo de api do artisan para inicialmente testar o servidor local

--- maiden-gate-backend/routes/api.php
+++ maiden-gate-backend/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/teste', function () {
+    return response()->json([
+        'message' => 'API do Maiden-Gate funcionando!'
+    ]);
+});
