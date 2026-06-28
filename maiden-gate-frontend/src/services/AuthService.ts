1399f20772c4e3ed30454cc55eba4e4e40b2b83f
Malvino <malvinogarcia2@gmail.com>
2026-06-28 10:29:36 -0600

    feat (AuthService.ts): implementa o AuthService para conversar com o backend e passar os dados para login e registro

--- maiden-gate-frontend/src/services/AuthService.ts
+++ maiden-gate-frontend/src/services/AuthService.ts
@@ -1,1 +1,31 @@
 // responsavel por conversar com o back
+import api from "./api";
+
+type LoginData = {
+  // dados obrigatórios para o login
+  name: string;
+  password: string;
+};
+
+type RegisterData = {
+  // dados obrigatórios para o registro
+  type: "master" | "player";
+  name: string;
+  password: string;
+};
+
+const authService = {
+  async login(data: LoginData) {
+    const response = await api.post("/login", data); // esperando o laravel responder
+
+    return response.data;
+  },
+
+  async register(data: RegisterData) {
+    const response = await api.post("/register", data);
+
+    return response.data;
+  },
+};
+
+export default authService;
