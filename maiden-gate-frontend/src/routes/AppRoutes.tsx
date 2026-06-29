236e35cae0904f7fdb67b656ebf477b174757231
Malvino <malvinogarcia2@gmail.com>
2026-06-29 16:30:00 -0600

    chore (HomePage.tsx): troca a página home de lugar

--- maiden-gate-frontend/src/routes/AppRoutes.tsx
+++ maiden-gate-frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import Login from "../pages/Login/Login";
+import HomePage from "../features/home/pages/HomePage";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
