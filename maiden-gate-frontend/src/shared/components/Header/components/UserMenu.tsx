72fbb917f092b7da4b3d32f1d77f0d51040772b4
Malvino <malvinogarcia2@gmail.com>
2026-06-29 14:58:32 -0600

    feat (UserMenu.tsx): implementa o botão de logout caso o usuário esteja logado

--- maiden-gate-frontend/src/shared/components/Header/components/UserMenu.tsx
+++ maiden-gate-frontend/src/shared/components/Header/components/UserMenu.tsx
@@ -1,3 +1,30 @@
-export default function userMenu() {
-  return false;
+import { useAuth } from "../../../../features/auth/hooks/useAuth";
+
+export default function UserMenu() {
+  const { user, logout } = useAuth();
+
+  return (
+    <div className="flex items-center gap-4">
+      <span className="text-sm text-stone-200">
+        Bem-vindo, <strong>{user?.name}</strong>
+      </span>
+
+      <button
+        onClick={logout}
+        className="
+          rounded-md
+          border
+          border-red-500/30
+          px-4
+          py-2
+          text-sm
+          text-red-400
+          transition
+          hover:bg-red-500/10
+        "
+      >
+        Sair
+      </button>
+    </div>
+  );
 }
