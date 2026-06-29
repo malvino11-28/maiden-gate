80ba32faafdc59f38d2b93723ffc2da580cfdef4
Malvino <malvinogarcia2@gmail.com>
2026-06-29 16:44:36 -0600

    feat (App.tsx): altera app para utilizar apenas o AppRoutes

--- maiden-gate-frontend/src/App.tsx
+++ maiden-gate-frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./shared/components/Header/Header";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return <Header />;
+  return <AppRoutes />;
 }
 
 export default App;
